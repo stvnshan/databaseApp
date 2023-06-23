c2a17bafc83179d9edd87d8ddb196cf2f4791d7a
Ethan Dobrowolski <edobrowo@uwaterloo.ca>
2023-06-22 22:41:58 -0400

    Update README, update test script, add sample.out file

--- backend/src/db/dbTest.js
+++ backend/src/db/dbTest.js
@@ -11,20 +11,37 @@ const {
 
 const main = async () => {
   try {
+  console.log();
+  console.log("Select all Victim IDs:");
   data = await selectAllVictimIDs();
   console.log(data);
-  data = await selectAllAgencyIDs();
-  console.log(data);
+
+  console.log();
+  console.log("Select information about Victim with ID 1:");
   data = await selectVictimByID(1);
   console.log(data);
+
+  console.log();
+  console.log("Select all Agency IDs:");
+  data = await selectAllAgencyIDs();
+  console.log(data);
+
+  console.log();
+  console.log("Select information about Agencies with IDs 70, 1055, and 3187:");
   data = await selectAgencyByID(70);
   console.log(data);
   data = await selectAgencyByID(1055);
   console.log(data);
   data = await selectAgencyByID(3187);
   console.log(data);
+
+  console.log();
+  console.log("Select all Incident IDs:");
   data = await selectAllIncidentIDs();
   console.log(data);
+
+  console.log();
+  console.log("Select information about Indicents with IDs 3, 4, 5, 8:");
   data = await selectIncidentByID(3);
   console.log(data);
   data = await selectIncidentByID(4);
@@ -33,6 +50,9 @@ const main = async () => {
   console.log(data);
   data = await selectIncidentByID(8);
   console.log(data);
+
+  console.log();
+  console.log("Select information about Agencies using the case-insensitive search term \"PoLIce\":");
   data = await selectAgencyByName("PoLIce");
   console.log(data);
 
