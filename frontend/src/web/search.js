ab0098138b2597c6b49a794d59997098b8f525df
Hani Ly <hanily112@gmail.com>
2023-07-10 17:57:08 -0400

    Added option to search Agencies  by number of shootings

--- frontend/src/web/search.js
+++ frontend/src/web/search.js
@@ -2,22 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Agency from "./Agency";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 const apiHost = process.env.REACT_APP_API_HOST;
 
 // Example Agency name: Abilene Police Department
 
 const Search = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [nameSearchQuery, setNameSearchQuery] = useState("");
+  const [shootLowSearchQuery, setShootLowSearchQuery] = useState(0);
+  const [shootHighSearchQuery, setShootHighSearchQuery] = useState(2147483647);
   const [searchResults, setSearchResults] = useState([]);
 
   // Called when user clicks Search
-  const searchAgencies = async () => {
+  const searchAgenciesByName = async () => {
     try {
       const response = await axios.get(
-        `${apiHost}/agency?name=${encodeURIComponent(searchQuery)}`
+        `${apiHost}/agency?name=${encodeURIComponent(nameSearchQuery)}`
       );
+      setSearchResults(response.data);
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const searchAgenciesByNumShoot = async () => {
+    try {
+      const getURL = `${apiHost}/agency?shootlow=${encodeURIComponent(
+        shootLowSearchQuery
+      )}&shoothigh=${encodeURIComponent(shootHighSearchQuery)}`;
+      console.log(getURL);
+      const response = await axios.get(getURL);
       setSearchResults(response.data);
       console.log(response);
     } catch (err) {
@@ -32,14 +48,55 @@ const Search = () => {
       </div>
       <h1>Washington Post -- Police Shooting Fatalities -- Agencies</h1>
 
-      <div>
-        <input
-          type="text"
-          placeholder="Search Agencies by name"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button onClick={searchAgencies}>Search</button>
+      <div className="card">
+        <h6 class="card-title">Search by Agency Name:</h6>
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text">Agency Name:</span>
+          </div>
+          <input
+            id="inputAgencyName"
+            className="form-control"
+            type="text"
+            placeholder="Search Agencies by name"
+            value={nameSearchQuery}
+            onChange={(e) => setNameSearchQuery(e.target.value)}
+          />
+          <button onClick={searchAgenciesByName}>Search</button>
+        </div>
+      </div>
+
+      <h6>Or</h6>
+
+      <div className="card">
+        <h6 className="card-title">Search by number of shootings:</h6>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">Min:</span>
+          </div>
+          <input
+            id="inputShootLow"
+            type="text"
+            placeholder="min. number of shootings"
+            value={shootLowSearchQuery}
+            onChange={(e) => setShootLowSearchQuery(e.target.value)}
+          />
+
+          <div className="input-group-prepend" style={{"margin-left": "2rem"}}>
+            <span className="input-group-text">Max:</span>
+          </div>
+          <input
+            id="inputShootHigh"
+            type="text"
+            placeholder="max. number of shootings"
+            value={shootHighSearchQuery}
+            onChange={(e) => setShootHighSearchQuery(e.target.value)}
+          />
+
+          <button style={{ "margin-left": "2rem" }} onClick={searchAgenciesByNumShoot}>
+            Search
+          </button>
+        </div>
       </div>
 
       <h2>Agencies</h2>
@@ -47,7 +104,7 @@ const Search = () => {
         {searchResults.length > 0 ? (
           <div>
             <p>Total results: {searchResults.length}</p>
-            <div className="card" style={{width: "100%"}}>
+            <div className="card" style={{ width: "100%" }}>
               <ul className="list-group list-group-flush">
                 {searchResults.map((agency) => (
                   <li key={agency.agencyid} className="list-group-item">
