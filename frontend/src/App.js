844549c5c3726fe5d422f173da7c122aec9d333f
stvnshan <83726215+stvnshan@users.noreply.github.com>
2023-07-10 08:50:36 -0400

    front end update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-const apiHost = process.env.REACT_APP_API_HOST;
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from  "./web/home";
+import Search from "./web/search";
+import Search2 from './web/search2';
+
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-
-  const searchAgencies = async () => {
-    try {
-      const response = await axios.get(`${apiHost}/agency?name=${encodeURIComponent(searchQuery)}`);
-      setSearchResults(response.data);
-      console.log(response);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  return (
-    <div>
-      <h1>Washington Post -- Police Shooting Fatalities -- Agencies</h1>
-
-      <div>
-        <input
-          type="text"
-          placeholder="Search Agencies by name"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button onClick={searchAgencies}>Search</button>
-      </div>
-
-      <h2>Agencies</h2>
-      <div>
-      {searchResults.length > 0 ?
-        (
-          <ul>
-            {searchResults.map((agency) => <li key={agency.agencyid}>{agency.agencyid}, {agency.agencyname}</li>)}
-          </ul>
-        ) : (
-          <h3></h3>
-        )}      </div>
+   return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/question/" element={<Search />} />
+          <Route path="/question2/" element={<Search2 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
