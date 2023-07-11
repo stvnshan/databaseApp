8195bcaba8956366ca494c440c50bf088c349dde
Ethan Dobrowolski <edobrowo@uwaterloo.ca>
2023-07-11 17:26:02 -0400

    Fix error spam from null incident IDs

--- frontend/src/web/search2.js
+++ frontend/src/web/search2.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Incident from './Incident';
@@ -151,7 +151,7 @@ const Search2 = () => {
             ))}
           </ul>
         ) : (
-          <h3></h3>
+          <h3>empty</h3>
         )}{" "}
       </div>
 
