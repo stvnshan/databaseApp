7876dbff1e78f85c6c31ab4aafb72cada28f9316
Ethan Dobrowolski <edobrowo@uwaterloo.ca>
2023-07-23 18:50:47 -0400

    Update navbar style

--- frontend/src/components/shared/nav.js
+++ frontend/src/components/shared/nav.js
@@ -5,16 +5,29 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const MainNav = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="nav-bar">
       <Container>
-        <Navbar.Brand href="/">PFSDP</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            alt="Red mark"
+            src="/logo.ico"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Redmark
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/incidents">Incidents</Nav.Link>
             <Nav.Link href="/agencies">Agencies</Nav.Link>
           </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text style={{fontSize: "12px"}}>
+            <em>The Washington Post police shooting data project</em>
+          </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
